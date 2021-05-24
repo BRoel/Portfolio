c1787c2c2d7c2b7bde71cc35dacdc46239349eed
Brennan Roel <brennanroel@gmail.com>
2021-05-24 12:24:42 -0500

    added button logic to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,25 @@ import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+
     const [click, setClick] = useState(false);
+
+    const [button, setButton] = useState(true);
+
     const handleClick = () => setClick(!click);
+
     const closeMobileMenu = () => setClick(false)
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton)
+
     return (
         <>
             <nav className="navbar">
@@ -37,6 +53,7 @@ function Navbar() {
                             </Link>
                         </li>
                     </ul>
+                    {button && <Button buttonStyle='btn--outline'>My Projects</Button>}
                 </div>
             </nav>
         </>
