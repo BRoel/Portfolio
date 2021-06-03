5135c9c624b3cbc1332bc97c7b77d8de7fc4b623
Brennan Roel <brennanroel@gmail.com>
2021-06-03 13:02:15 -0500

    linked cards with home page

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import CardItem from './CardItem'
 
 function Cards() {
     return (
         <div className="cards">
             <h1>Cards</h1>
+            <div className="cards__container">
+                <div className="cards__wrapper">
+                    <ul className="cards__items">
+                        <CardItem />
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../App.css'
+import Cards from '../Cards'
 import MainSection from '../MainSection'
 
 
@@ -7,6 +8,7 @@ function Home() {
     return (
         <>
          <MainSection />
+         <Cards />
         </>
     )
 }
