1ff2c40379b71138bb0477f39d05cecba3c840aa
Brennan Roel <brennanroel@gmail.com>
2021-06-03 15:33:12 -0500

    added images and cards

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -5,13 +5,13 @@ function CardItem(props) {
 
     return (
         <>
-            <li className="cards__item">
-                <Link className="cards__item__link" to={props.path}>
-                    <figure className="cards__item__pic-wrap" data-category={props.label}>
-                        <img src={props.src} alt="Project Image" className="cards__item__image" />
+            <li className='cards__item'>
+                <Link className='cards__item__link' to={props.path}>
+                    <figure className='cards__item__pic-wrap' data-category={props.label}>
+                        <img src={props.src} alt='Project' className='cards__item__img' />
                     </figure>
-                    <div className="cards__item__info">
-                        <h5 className="cards__item__text">{props.text}</h5>
+                    <div className='cards__item__info'>
+                        <h5 className='cards__item__text'>{props.text}</h5>
                     </div>
                 </Link>
             </li>
