0a5c642485b98b03fc9366bc5316c1737c3e59cb
Brennan Roel <brennanroel@gmail.com>
2021-06-15 10:33:47 -0500

    added Calorie Counter project page

---
+++ src/components/pages/CalorieCounter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../App.css'
+import Footer from '../Footer'
+
+function CalorieCounter() {
+    return (
+        <>
+            <h1>Calorie Counter</h1>
+            <Footer/>
+        </>
+    );
+}
+
+export default CalorieCounter
