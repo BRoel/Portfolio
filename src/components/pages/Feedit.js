871ea1f7888140fd6cdd22e8cee144a5ef13189e
Brennan Roel <brennanroel@gmail.com>
2021-06-15 10:31:41 -0500

    added Feed-it project page

---
+++ src/components/pages/Feedit.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../App.css'
+import Footer from '../Footer'
+
+function Feedit() {
+    return (
+        <>
+            <h1>Feedit</h1>
+            <Footer/>
+        </>
+    );
+}
+
+export default Feedit
