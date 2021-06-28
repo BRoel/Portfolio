e233c03565f41ed5cca56b91a0afbfdae712813f
Brennan Roel <brennanroel@gmail.com>
2021-06-28 15:02:30 -0500

    removed logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
             <nav className="navbar" >
                 <div className="navbar-container">
                     <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-                        BR <i class="far fa-comment-dots"/>
+                        BR
                     </Link>
                     <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
